0ad492be5ed265058408ba01124f9fe4af100f4a
Balkov Arthur <balkov_arthur@mail.ru>
2020-01-12 16:38:15 +0300

    Done

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/AirArto/otus
 
 go 1.13
 
-require github.com/AirArto/hw-1 v0.0.0-20200106044242-5192691c34c7
+require (
+	github.com/AirArto/hw-1 v0.0.0-20200106044242-5192691c34c7
+	github.com/AirArto/hw-3 v0.0.0-20200109000112-b80469529a48
+)
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	hw "github.com/AirArto/hw-1"
+	hw "github.com/AirArto/hw-3"
 )
 
 func init() {
@@ -12,7 +12,10 @@ func init() {
 }
 
 func main() {
-	data, err := hw.Do()
+	UnpackedArr := "oNe \\seven\\tWo, tHree, fOur,oNe \\seven\\tWo,   seven  tHree, fOur,//six nine, two, three-foUr/seven/, nine.../seven///eight//'.four'" +
+		" nine.nine.nine.nine.nine//nine eight//. eight  seven six five oNe \\\\tWo, tHree, fOur/seven/six nine.,;',nine three'/seven///eight//>/seven/'.four123nine" +
+		"//eight////eight//six five five;five//eight//eight//;<>0]1354six//.six['six///eight//five';five]]['six*five//eight,cat,cat,... dog some more random words lorem ipsum"
+	data, err := hw.Do(&UnpackedArr)
 	if err == nil {
 		fmt.Println("Requested data:", data)
 	} else {
