b1d74cda7cae78c1bfbefa269dd3bddc73c8b0ac
Balkov Arthur <balkov_arthur@mail.ru>
2020-02-24 23:58:38 +0300

    Done

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module github.com/AirArto/otus
 
 go 1.13
 
-require github.com/AirArto/hw-1 v0.0.0-20200106044242-5192691c34c7
+require (
+	github.com/AirArto/hw-6 v0.0.0-20200224204524-c8553546c337
+	github.com/cheggaaa/pb v2.0.7+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
+	gopkg.in/VividCortex/ewma.v1 v1.1.1 // indirect
+	gopkg.in/cheggaaa/pb.v2 v2.0.7 // indirect
+	gopkg.in/fatih/color.v1 v1.7.0 // indirect
+	gopkg.in/mattn/go-colorable.v0 v0.1.0 // indirect
+	gopkg.in/mattn/go-isatty.v0 v0.0.4 // indirect
+	gopkg.in/mattn/go-runewidth.v0 v0.0.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
 
-	hw "github.com/AirArto/hw-1"
+	hw "github.com/AirArto/hw-6"
+)
+
+var (
+	from   string
+	to     string
+	limit  int
+	offset int
 )
 
 func init() {
-	//
+	flag.StringVar(&from, "from", "", "file to read")
+	flag.StringVar(&to, "to", "", "file to write")
+	flag.IntVar(&limit, "limit", 0, "limit to copy in bytes. By default limit = fileSize - offset")
+	flag.IntVar(&offset, "offset", 0, "offset to copy in bytes.")
 }
 
 func main() {
-	data, err := hw.Do()
-	if err == nil {
-		fmt.Println("Requested data:", data)
-	} else {
-		log.Println("Data displaying error: \n\t", err)
+	flag.Parse()
+	_, err := hw.Copy(from, to, limit, offset)
+	if err != nil {
+		fmt.Println(err)
 	}
 }
