bb792c546bb7dd00ea24c31c27dd7d64fda4481e
Balkov Arthur <balkov_arthur@mail.ru>
2020-03-17 02:09:47 +0300

    Done

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/AirArto/otus
 
 go 1.13
 
-require github.com/AirArto/hw-1 v0.0.0-20200106044242-5192691c34c7
+require github.com/AirArto/hw-7 v0.0.0-20200316225832-7f3e2d488b97
--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
-	hw "github.com/AirArto/hw-1"
+	hw "github.com/AirArto/hw-7"
 )
 
-func init() {
-	//
-}
-
 func main() {
-	data, err := hw.Do()
-	if err == nil {
-		fmt.Println("Requested data:", data)
-	} else {
-		log.Println("Data displaying error: \n\t", err)
+	args := os.Args
+	if len(args) < 3 {
+		log.Fatalf("Error, need more args")
 	}
+
+	dir := args[1]
+	cmd := args[2:]
+	absDir, err := filepath.Abs(dir)
+	if err != nil {
+		log.Fatalf("Env directory error: %v", err)
+	}
+
+	env, err := hw.ReadDir(absDir)
+	if err != nil {
+		log.Fatalf("Env directory error: %v", err)
+	}
+	statusCode := hw.RunCmd(cmd, env)
+
+	os.Exit(statusCode)
 }
